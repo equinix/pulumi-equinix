ffad754750ef1c10985b7cd46264ee009e03d62a
ocobleseqx <oscar.cobles@eu.equinix.com>
2023-03-23 16:55:03 +0100

    equinix_metal_ip_attachment example
    Signed-off-by: ocobleseqx <oscar.cobles@eu.equinix.com>

---
+++ examples/metal/ip_attachment/typescript/index.ts
@@ -0,0 +1,12 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as equinix from "@equinix/pulumi-equinix";
+
+const config = new pulumi.Config();
+const deviceId = config.require("deviceId");
+const subnetCidr = config.get("subnetCidr") || "147.229.10.152/31";
+const ipAttachResource = new equinix.metal.IpAttachment("ipAttach", {
+    deviceId: deviceId,
+    cidrNotation: subnetCidr,
+});
+export const ipAttach = ipAttachResource.id;
+export const ipNetmask = ipAttachResource.netmask;
