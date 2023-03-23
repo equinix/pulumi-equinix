5b8f284676e12fc9bf70b06b6e6dea98e4906732
ocobleseqx <oscar.cobles@eu.equinix.com>
2023-03-23 18:53:28 +0100

    equinix_metal_ssh_key example
    Signed-off-by: ocobleseqx <oscar.cobles@eu.equinix.com>

---
+++ examples/metal/ssh_key/typescript/index.ts
@@ -0,0 +1,9 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as equinix from "@equinix/pulumi-equinix";
+import * as fs from "fs";
+
+const sshKey = new equinix.metal.SshKey("sshKey", {
+    name: "johnKent",
+    publicKey: fs.readFileSync("/Users/John/.ssh/metal_rsa.pub"),
+});
+export const sshKeyId = sshKey.id;
