ccec0d2ffa09c430c00d71b541199d2ce615b1c1
ocobleseqx <oscar.cobles@eu.equinix.com>
2023-03-23 18:41:15 +0100

    equinix_metal_reserved_ip_block example
    Signed-off-by: ocobleseqx <oscar.cobles@eu.equinix.com>

---
+++ examples/metal/reserved_ip_block/typescript/index.ts
@@ -0,0 +1,16 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as equinix from "@equinix/pulumi-equinix";
+
+const config = new pulumi.Config();
+const projectId = config.require("projectId");
+const metro = config.get("metro") || "FR";
+const type = config.get("type") || "public_ipv4";
+const quantity = config.getNumber("quantity") || 1;
+const ipBlock = new equinix.metal.ReservedIpBlock("ipBlock", {
+    projectId: projectId,
+    type: "public_ipv4",
+    quantity: quantity,
+    metro: metro,
+});
+export const ipBlockId = ipBlock.id;
+export const ipBlockSubent = ipBlock.cidrNotation;
