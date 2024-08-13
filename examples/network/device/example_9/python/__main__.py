b234773f03c7749e962a552f381d054fb997c662
GitHub <noreply@github.com>
2024-08-13 16:11:18 +0000

    make tfgen

--- examples/network/device/example_9/python/__main__.py
+++ examples/network/device/example_9/python/__main__.py
@@ -19,27 +19,27 @@ panw_cluster = equinix.networkedge.Device("panwCluster",
     version="11.1.3",
     interface_count=10,
     core_count=2,
-    ssh_key=equinix.networkedge.DeviceSshKeyArgs(
-        username="test",
-        key_name="test-key",
-    ),
+    ssh_key={
+        "username": "test",
+        "key_name": "test-key",
+    },
     acl_template_id="0bff6e05-f0e7-44cd-804a-25b92b835f8b",
-    cluster_details=equinix.networkedge.DeviceClusterDetailsArgs(
-        cluster_name="tf-panw-cluster",
-        node0=equinix.networkedge.DeviceClusterDetailsNode0Args(
-            vendor_configuration=equinix.networkedge.DeviceClusterDetailsNode0VendorConfigurationArgs(
-                hostname="panw-node0",
-                panorama_ip_address="x.x.x.x",
-                panorama_auth_key="xxxxxxxxxxx",
-            ),
-            license_token="licenseToken",
-        ),
-        node1=equinix.networkedge.DeviceClusterDetailsNode1Args(
-            vendor_configuration=equinix.networkedge.DeviceClusterDetailsNode1VendorConfigurationArgs(
-                hostname="panw-node1",
-                panorama_ip_address="x.x.x.x",
-                panorama_auth_key="xxxxxxxxxxx",
-            ),
-            license_token="licenseToken",
-        ),
-    ))
+    cluster_details={
+        "cluster_name": "tf-panw-cluster",
+        "node0": {
+            "vendor_configuration": {
+                "hostname": "panw-node0",
+                "panorama_ip_address": "x.x.x.x",
+                "panorama_auth_key": "xxxxxxxxxxx",
+            },
+            "license_token": "licenseToken",
+        },
+        "node1": {
+            "vendor_configuration": {
+                "hostname": "panw-node1",
+                "panorama_ip_address": "x.x.x.x",
+                "panorama_auth_key": "xxxxxxxxxxx",
+            },
+            "license_token": "licenseToken",
+        },
+    })
