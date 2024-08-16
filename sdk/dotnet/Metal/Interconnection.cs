63c54dc2a5aa353eff84c8796ddf45a71ee171d6
GitHub <noreply@github.com>
2024-08-16 16:11:39 +0000

    make build_sdks

--- sdk/dotnet/Metal/Interconnection.cs
+++ sdk/dotnet/Metal/Interconnection.cs
@@ -77,6 +77,172 @@ namespace Pulumi.Equinix.Metal
     ///     };
     /// });
     /// ```
+    /// ### example shared metal fabric connection from fcr
+    /// ```csharp
+    /// using System.Collections.Generic;
+    /// using System.Linq;
+    /// using Pulumi;
+    /// using Equinix = Pulumi.Equinix;
+    /// 
+    /// return await Deployment.RunAsync(() =&gt; 
+    /// {
+    ///     var example1 = new Equinix.Metal.Vlan("example1", new()
+    ///     {
+    ///         ProjectId = myProjectId,
+    ///         Metro = "SV",
+    ///     });
+    /// 
+    ///     var example = new Equinix.Metal.Interconnection("example", new()
+    ///     {
+    ///         Name = "tf-port-to-metal-legacy",
+    ///         ProjectId = myProjectId,
+    ///         Metro = "SV",
+    ///         Redundancy = "primary",
+    ///         Type = "shared_port_vlan",
+    ///         ContactEmail = "username@example.com",
+    ///         Vlans = new[]
+    ///         {
+    ///             example1.Vxlan,
+    ///         },
+    ///     });
+    /// 
+    ///     var exampleConnection = new Equinix.Fabric.Connection("exampleConnection", new()
+    ///     {
+    ///         Name = "tf-NIMF-metal-2-aws-legacy",
+    ///         Type = "IP_VC",
+    ///         Notifications = new[]
+    ///         {
+    ///             new Equinix.Fabric.Inputs.ConnectionNotificationArgs
+    ///             {
+    ///                 Type = Equinix.Fabric.NotificationsType.All,
+    ///                 Emails = "sername@example.com",
+    ///             },
+    ///         },
+    ///         Project = new Equinix.Fabric.Inputs.ConnectionProjectArgs
+    ///         {
+    ///             ProjectId = fabricProjectId,
+    ///         },
+    ///         Bandwidth = 200,
+    ///         Order = new Equinix.Fabric.Inputs.ConnectionOrderArgs
+    ///         {
+    ///             PurchaseOrderNumber = "1-323292",
+    ///         },
+    ///         ASide = new Equinix.Fabric.Inputs.ConnectionASideArgs
+    ///         {
+    ///             AccessPoint = new Equinix.Fabric.Inputs.ConnectionASideAccessPointArgs
+    ///             {
+    ///                 Type = "CLOUD_ROUTER",
+    ///                 Router = new Equinix.Fabric.Inputs.ConnectionASideAccessPointRouterArgs
+    ///                 {
+    ///                     Uuid = cloudRouterUuid,
+    ///                 },
+    ///             },
+    ///         },
+    ///         ZSide = new Equinix.Fabric.Inputs.ConnectionZSideArgs
+    ///         {
+    ///             AccessPoint = new Equinix.Fabric.Inputs.ConnectionZSideAccessPointArgs
+    ///             {
+    ///                 Type = "METAL_NETWORK",
+    ///                 AuthenticationKey = example.AuthorizationCode,
+    ///             },
+    ///         },
+    ///     });
+    /// 
+    /// });
+    /// ```
+    /// ### example shared metal fabric connection to csp
+    /// ```csharp
+    /// using System.Collections.Generic;
+    /// using System.Linq;
+    /// using Pulumi;
+    /// using Equinix = Pulumi.Equinix;
+    /// 
+    /// return await Deployment.RunAsync(() =&gt; 
+    /// {
+    ///     var zside = Equinix.Fabric.GetServiceProfiles.Invoke(new()
+    ///     {
+    ///         Filter = new Equinix.Fabric.Inputs.GetServiceProfilesFilterInputArgs
+    ///         {
+    ///             Property = "/name",
+    ///             Operator = "=",
+    ///             Values = new[]
+    ///             {
+    ///                 "AWS Direct Connect",
+    ///             },
+    ///         },
+    ///     });
+    /// 
+    ///     var example1 = new Equinix.Metal.Vlan("example1", new()
+    ///     {
+    ///         ProjectId = myProjectId,
+    ///         Metro = "SV",
+    ///     });
+    /// 
+    ///     var example = new Equinix.Metal.Interconnection("example", new()
+    ///     {
+    ///         Name = "tf-port-to-metal-legacy",
+    ///         ProjectId = myProjectId,
+    ///         Metro = "SV",
+    ///         Redundancy = "primary",
+    ///         Type = "shared_port_vlan",
+    ///         ContactEmail = "username@example.com",
+    ///         Vlans = new[]
+    ///         {
+    ///             example1.Vxlan,
+    ///         },
+    ///     });
+    /// 
+    ///     var exampleConnection = new Equinix.Fabric.Connection("exampleConnection", new()
+    ///     {
+    ///         Name = "tf-NIMF-metal-2-aws-legacy",
+    ///         Type = Equinix.Fabric.ConnectionType.EVPL,
+    ///         Notifications = new[]
+    ///         {
+    ///             new Equinix.Fabric.Inputs.ConnectionNotificationArgs
+    ///             {
+    ///                 Type = Equinix.Fabric.NotificationsType.All,
+    ///                 Emails = "sername@example.com",
+    ///             },
+    ///         },
+    ///         Project = new Equinix.Fabric.Inputs.ConnectionProjectArgs
+    ///         {
+    ///             ProjectId = fabricProjectId,
+    ///         },
+    ///         Bandwidth = 200,
+    ///         Order = new Equinix.Fabric.Inputs.ConnectionOrderArgs
+    ///         {
+    ///             PurchaseOrderNumber = "1-323292",
+    ///         },
+    ///         ASide = new Equinix.Fabric.Inputs.ConnectionASideArgs
+    ///         {
+    ///             AccessPoint = new Equinix.Fabric.Inputs.ConnectionASideAccessPointArgs
+    ///             {
+    ///                 Type = "METAL_NETWORK",
+    ///                 AuthenticationKey = example.AuthorizationCode,
+    ///             },
+    ///         },
+    ///         ZSide = new Equinix.Fabric.Inputs.ConnectionZSideArgs
+    ///         {
+    ///             AccessPoint = new Equinix.Fabric.Inputs.ConnectionZSideAccessPointArgs
+    ///             {
+    ///                 Type = Equinix.Fabric.AccessPointType.SP,
+    ///                 AuthenticationKey = awsAccountId,
+    ///                 SellerRegion = "us-west-1",
+    ///                 Profile = new Equinix.Fabric.Inputs.ConnectionZSideAccessPointProfileArgs
+    ///                 {
+    ///                     Type = Equinix.Fabric.ProfileType.L2Profile,
+    ///                     Uuid = zside.Apply(getServiceProfilesResult =&gt; getServiceProfilesResult.Id),
+    ///                 },
+    ///                 Location = new Equinix.Fabric.Inputs.ConnectionZSideAccessPointLocationArgs
+    ///                 {
+    ///                     MetroCode = Equinix.Metro.SiliconValley,
+    ///                 },
+    ///             },
+    ///         },
+    ///     });
+    /// 
+    /// });
+    /// ```
     /// </summary>
     [EquinixResourceType("equinix:metal/interconnection:Interconnection")]
     public partial class Interconnection : global::Pulumi.CustomResource
