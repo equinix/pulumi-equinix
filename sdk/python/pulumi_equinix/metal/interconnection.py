f771168a718d7b7631c867654739de1875db3f30
GitHub <noreply@github.com>
2024-08-19 00:24:24 +0000

    make build_sdks

--- sdk/python/pulumi_equinix/metal/interconnection.py
+++ sdk/python/pulumi_equinix/metal/interconnection.py
@@ -678,6 +678,107 @@ class Interconnection(pulumi.CustomResource):
         pulumi.export("connectionStatus", connection.status)
         pulumi.export("connectionTokens", connection.service_tokens)
         ```
+        ### example shared metal fabric connection from fcr
+        ```python
+        import pulumi
+        import pulumi_equinix as equinix
+
+        example1 = equinix.metal.Vlan("example1",
+            project_id=my_project_id,
+            metro="SV")
+        example = equinix.metal.Interconnection("example",
+            name="tf-port-to-metal-legacy",
+            project_id=my_project_id,
+            metro="SV",
+            redundancy="primary",
+            type="shared_port_vlan",
+            contact_email="username@example.com",
+            vlans=[example1.vxlan])
+        example_connection = equinix.fabric.Connection("exampleConnection",
+            name="tf-NIMF-metal-2-aws-legacy",
+            type="IP_VC",
+            notifications=[{
+                "type": equinix.fabric.NotificationsType.ALL,
+                "emails": "sername@example.com",
+            }],
+            project={
+                "project_id": fabric_project_id,
+            },
+            bandwidth=200,
+            order={
+                "purchase_order_number": "1-323292",
+            },
+            a_side={
+                "access_point": {
+                    "type": "CLOUD_ROUTER",
+                    "router": {
+                        "uuid": cloud_router_uuid,
+                    },
+                },
+            },
+            z_side={
+                "access_point": {
+                    "type": "METAL_NETWORK",
+                    "authentication_key": example.authorization_code,
+                },
+            })
+        ```
+        ### example shared metal fabric connection to csp
+        ```python
+        import pulumi
+        import pulumi_equinix as equinix
+
+        zside = equinix.fabric.get_service_profiles_output(filter={
+            "property": "/name",
+            "operator": "=",
+            "values": ["AWS Direct Connect"],
+        })
+        example1 = equinix.metal.Vlan("example1",
+            project_id=my_project_id,
+            metro="SV")
+        example = equinix.metal.Interconnection("example",
+            name="tf-port-to-metal-legacy",
+            project_id=my_project_id,
+            metro="SV",
+            redundancy="primary",
+            type="shared_port_vlan",
+            contact_email="username@example.com",
+            vlans=[example1.vxlan])
+        example_connection = equinix.fabric.Connection("exampleConnection",
+            name="tf-NIMF-metal-2-aws-legacy",
+            type=equinix.fabric.ConnectionType.EVPL,
+            notifications=[{
+                "type": equinix.fabric.NotificationsType.ALL,
+                "emails": "sername@example.com",
+            }],
+            project={
+                "project_id": fabric_project_id,
+            },
+            bandwidth=200,
+            order={
+                "purchase_order_number": "1-323292",
+            },
+            a_side={
+                "access_point": {
+                    "type": "METAL_NETWORK",
+                    "authentication_key": example.authorization_code,
+                },
+            },
+            z_side={
+                "access_point": {
+                    "type": equinix.fabric.AccessPointType.SP,
+                    "authentication_key": aws_account_id,
+                    "seller_region": "us-west-1",
+                    "profile": {
+                        "type": equinix.fabric.ProfileType.L2_PROFILE,
+                        "uuid": zside.id,
+                    },
+                    "location": {
+                        "metro_code": equinix.Metro.SILICON_VALLEY,
+                    },
+                },
+            })
+        ```
 
         :param str resource_name: The name of the resource.
         :param pulumi.ResourceOptions opts: Options for the resource.
@@ -757,6 +858,107 @@ class Interconnection(pulumi.CustomResource):
             service_token_type="a_side")
         pulumi.export("connectionStatus", connection.status)
         pulumi.export("connectionTokens", connection.service_tokens)
+        ```
+        ### example shared metal fabric connection from fcr
+        ```python
+        import pulumi
+        import pulumi_equinix as equinix
+
+        example1 = equinix.metal.Vlan("example1",
+            project_id=my_project_id,
+            metro="SV")
+        example = equinix.metal.Interconnection("example",
+            name="tf-port-to-metal-legacy",
+            project_id=my_project_id,
+            metro="SV",
+            redundancy="primary",
+            type="shared_port_vlan",
+            contact_email="username@example.com",
+            vlans=[example1.vxlan])
+        example_connection = equinix.fabric.Connection("exampleConnection",
+            name="tf-NIMF-metal-2-aws-legacy",
+            type="IP_VC",
+            notifications=[{
+                "type": equinix.fabric.NotificationsType.ALL,
+                "emails": "sername@example.com",
+            }],
+            project={
+                "project_id": fabric_project_id,
+            },
+            bandwidth=200,
+            order={
+                "purchase_order_number": "1-323292",
+            },
+            a_side={
+                "access_point": {
+                    "type": "CLOUD_ROUTER",
+                    "router": {
+                        "uuid": cloud_router_uuid,
+                    },
+                },
+            },
+            z_side={
+                "access_point": {
+                    "type": "METAL_NETWORK",
+                    "authentication_key": example.authorization_code,
+                },
+            })
+        ```
+        ### example shared metal fabric connection to csp
+        ```python
+        import pulumi
+        import pulumi_equinix as equinix
+
+        zside = equinix.fabric.get_service_profiles_output(filter={
+            "property": "/name",
+            "operator": "=",
+            "values": ["AWS Direct Connect"],
+        })
+        example1 = equinix.metal.Vlan("example1",
+            project_id=my_project_id,
+            metro="SV")
+        example = equinix.metal.Interconnection("example",
+            name="tf-port-to-metal-legacy",
+            project_id=my_project_id,
+            metro="SV",
+            redundancy="primary",
+            type="shared_port_vlan",
+            contact_email="username@example.com",
+            vlans=[example1.vxlan])
+        example_connection = equinix.fabric.Connection("exampleConnection",
+            name="tf-NIMF-metal-2-aws-legacy",
+            type=equinix.fabric.ConnectionType.EVPL,
+            notifications=[{
+                "type": equinix.fabric.NotificationsType.ALL,
+                "emails": "sername@example.com",
+            }],
+            project={
+                "project_id": fabric_project_id,
+            },
+            bandwidth=200,
+            order={
+                "purchase_order_number": "1-323292",
+            },
+            a_side={
+                "access_point": {
+                    "type": "METAL_NETWORK",
+                    "authentication_key": example.authorization_code,
+                },
+            },
+            z_side={
+                "access_point": {
+                    "type": equinix.fabric.AccessPointType.SP,
+                    "authentication_key": aws_account_id,
+                    "seller_region": "us-west-1",
+                    "profile": {
+                        "type": equinix.fabric.ProfileType.L2_PROFILE,
+                        "uuid": zside.id,
+                    },
+                    "location": {
+                        "metro_code": equinix.Metro.SILICON_VALLEY,
+                    },
+                },
+            })
         ```
 
         :param str resource_name: The name of the resource.
