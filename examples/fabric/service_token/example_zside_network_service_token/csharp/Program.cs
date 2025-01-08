b2683e97298c76a59df0e811ace06486febe1c01
GitHub <noreply@github.com>
2025-01-08 00:39:18 +0000

    make tfgen

---
+++ examples/fabric/service_token/example_zside_network_service_token/csharp/Program.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Linq;
+using Pulumi;
+using Equinix = Pulumi.Equinix;
+
+return await Deployment.RunAsync(() => 
+{
+    var test = new Equinix.Fabric.ServiceToken("test", new()
+    {
+        Type = "VC_TOKEN",
+        Description = "Zside Network Service Token",
+        ExpirationDateTime = "2025-01-18T06:43:49.986Z",
+        ServiceTokenConnections = new[]
+        {
+            new Equinix.Fabric.Inputs.ServiceTokenServiceTokenConnectionArgs
+            {
+                Type = "EVPL_VC",
+                SupportedBandwidths = new[]
+                {
+                    50,
+                    200,
+                    10000,
+                },
+                ZSides = new[]
+                {
+                    new Equinix.Fabric.Inputs.ServiceTokenServiceTokenConnectionZSideArgs
+                    {
+                        AccessPointSelectors = new[]
+                        {
+                            new Equinix.Fabric.Inputs.ServiceTokenServiceTokenConnectionZSideAccessPointSelectorArgs
+                            {
+                                Type = "NETWORK",
+                                Network = new Equinix.Fabric.Inputs.ServiceTokenServiceTokenConnectionZSideAccessPointSelectorNetworkArgs
+                                {
+                                    Uuid = "<network_uuid>",
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+        Notifications = new[]
+        {
+            new Equinix.Fabric.Inputs.ServiceTokenNotificationArgs
+            {
+                Type = "ALL",
+                Emails = new[]
+                {
+                    "example@equinix.com",
+                },
+            },
+        },
+    });
+
+});
+
---
+++ examples/fabric/service_token/example_zside_network_service_token/java/src/main/java/generated_program/App.java
@@ -0,0 +1,49 @@
+package generated_program;
+
+import com.pulumi.Context;
+import com.pulumi.Pulumi;
+import com.pulumi.core.Output;
+import com.pulumi.equinix.fabric.ServiceToken;
+import com.pulumi.equinix.fabric.ServiceTokenArgs;
+import com.pulumi.equinix.fabric.inputs.ServiceTokenServiceTokenConnectionArgs;
+import com.pulumi.equinix.fabric.inputs.ServiceTokenNotificationArgs;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) {
+        Pulumi.run(App::stack);
+    }
+
+    public static void stack(Context ctx) {
+        var test = new ServiceToken("test", ServiceTokenArgs.builder()
+            .type("VC_TOKEN")
+            .description("Zside Network Service Token")
+            .expirationDateTime("2025-01-18T06:43:49.986Z")
+            .serviceTokenConnections(ServiceTokenServiceTokenConnectionArgs.builder()
+                .type("EVPL_VC")
+                .supportedBandwidths(                
+                    50,
+                    200,
+                    10000)
+                .zSides(ServiceTokenServiceTokenConnectionZSideArgs.builder()
+                    .accessPointSelectors(ServiceTokenServiceTokenConnectionZSideAccessPointSelectorArgs.builder()
+                        .type("NETWORK")
+                        .network(ServiceTokenServiceTokenConnectionZSideAccessPointSelectorNetworkArgs.builder()
+                            .uuid("<network_uuid>")
+                            .build())
+                        .build())
+                    .build())
+                .build())
+            .notifications(ServiceTokenNotificationArgs.builder()
+                .type("ALL")
+                .emails("example@equinix.com")
+                .build())
+            .build());
+
+    }
+}
