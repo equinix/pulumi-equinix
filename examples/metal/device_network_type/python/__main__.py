8afd8aec8d5f5e28e817420ba7dfcee2fcf04e61
Ng Tek In <tng1@ap.equinix.com>
2026-09-03 20:16:34 +0800

    Remove metal examples

--- examples/metal/device_network_type/python/__main__.py
+++
@@ -1,8 +0,0 @@
-import pulumi
-
-config = pulumi.Config()
-device_id = config.require("deviceId")
-network_type = config.get("networkType")
-if network_type is None:
-    network_type = "hybrid"
-pulumi.export("deviceNetworkId", device_network["id"])
--- examples/metal/device_network_type/typescript/index.ts
+++
@@ -1,6 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-
-const config = new pulumi.Config();
-const deviceId = config.require("deviceId");
-const networkType = config.get("networkType") || "hybrid";
-export const deviceNetworkId = deviceNetwork.id;
